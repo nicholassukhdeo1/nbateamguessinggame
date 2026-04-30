a5c78857f20f01c45e98a5a3ecf4509ef6ea419f
TheCreator771 <112454325+TheCreator771@users.noreply.github.com>
2026-04-29 22:46:36 -0700

    use this

--- Team.h
+++ Team.h
@@ -1,1 +1,15 @@
 #pragma once
+#include <string>
+using namespace std;
+
+class Team {
+public:
+    string name, conference, division, primaryColor, secondaryColor;
+    int championships, founded;
+
+    Team(string n, string conf, string div, string pc, string sc, int champs, int f)
+        : name(n), conference(conf), division(div),
+        primaryColor(pc), secondaryColor(sc),
+        championships(champs), founded(f) {
+    }
+};
